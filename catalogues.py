eb7a595d7481e65465993e2ef3854f1704d0f5c7
Teresa Huertas <teresahuertas@gmail.com>
2023-10-09 17:57:44 +0100

    Add a function that read catalogue files

--- catalogues.py
+++ catalogues.py
@@ -29,4 +29,40 @@ class Catalogue:
                 self.uf =
 
         return
-            
+    
+
+    def read_catalogue_file(self, name):
+        """
+        Function to read the catalogue file and return a pandas dataframe
+        
+        Parameters
+        ----------
+        name : str
+            Name of the catalogue file to read
+
+        Returns
+        -------
+        catalogue : pandas dataframe
+            Dataframe with the catalogue information
+        """
+        try:
+            file_format = [(0, 1), (1, 4), (4, 19), (19, 29), 
+                           (29, 37), (37, 45), (45, 50), (50, 60), 
+                           (60, 75), (75, 79), (79, 94), (94, 100)]
+            data = pd.read_fwf(name, colspecs=file_format, header=None)
+            data = data[5:]
+            data.drop([1, 4, 5, 6, 7, 9], axis=1, inplace=True)
+            data.rename(columns={0: 'Status',
+                                 2: 'Species',
+                                 3: 'Freq[MHz]',
+                                 8: 'Upper',
+                                 10: 'Lower',
+                                 11: 'Origin'}, inplace=True)
+            print(name + ' data read successfully')
+            return data
+        except FileNotFoundError:
+            print('File not found')
+            return None
+        except Exception as e:
+            print('Error reading {e}')
+            return None
