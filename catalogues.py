2a6f630e80b584475a65542422d46147d8675164
Teresa Huertas <teresahuertas@gmail.com>
2023-10-09 17:46:30 +0100

    Add file paths

--- catalogues.py
+++ catalogues.py
@@ -1,1 +1,8 @@
 import pandas as pd
+import os
+
+os.path.abspath(os.getcwd())
+
+# Paths to catalogues defined from the current directory
+cat_path = 'RadioChem/Source_Catalogues/'
+results_path = 'RadioChem/Results/'
