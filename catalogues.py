e2beb05b2d75dfaf3c898bb1d25c08322c199c03
Teresa Huertas <teresahuertas@gmail.com>
2023-10-09 17:51:34 +0100

    Initiate catalogue class

--- catalogues.py
+++ catalogues.py
@@ -1,8 +1,32 @@
 import pandas as pd
 import os
 
+
 os.path.abspath(os.getcwd())
 
-# Paths to catalogues defined from the current directory
-cat_path = 'RadioChem/Source_Catalogues/'
-results_path = 'RadioChem/Results/'
+
+class Catalogue:
+    """
+    Class to read, classify and analyse the obseved lines in PNe
+    """
+
+    # Paths to catalogues defined from the current directory
+    cat_path = 'RadioChem/Source_Catalogues/'
+    results_path = 'RadioChem/Results/'
+
+    def __init__(self, name=None):
+        if not name:
+            print('Line catalogue created with any source specified')
+        elif name == None:
+            self.catalogue = None
+        else:
+            self.catalogue = read_catalogue_file(name)
+            if name == 'rrls':
+                self.catalogue = self.catalogue
+            else:
+                self.rrls = 
+                self.molecules =
+                self.uf =
+
+        return
+            
