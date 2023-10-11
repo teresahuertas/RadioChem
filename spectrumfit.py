32486f82a0870d788d49ad862795e71307e046dd
Teresa Huertas <teresahuertas@gmail.com>
2023-10-11 14:49:44 +0100

    Create a function to read the spectrum from file

--- spectrumfit.py
+++ spectrumfit.py
@@ -8,3 +8,30 @@ from astropy.constants import si
 from specutils import Spectrum1D, SpectralRegion
 from specutils.fitting import fit_generic_continuum, find_lines_derivative, find_lines_threshold
 from specutils.manipulation import noise_region_uncertainty
+
+
+def read_spectrum(filename):
+    """
+    Function to read a spectrum from a file
+
+    Parameters
+    ----------
+    filename : str
+        Name of the file to read
+
+    Returns
+    -------
+    data : pandas.DataFrame
+        Data read from the file
+    """
+    try:
+        data = pd.read_csv(filename, sep='\s+', header=None, names=['rx(km/s)', 'ry(Tmb)'])
+        data.astype({'rx(km/s)': 'float64', 'ry(Tmb)': 'float64'}).dtypes
+        data = data[data['ry(Tmb)'] > -1.0]
+        return data
+    except FileNotFoundError:
+        print(f'File {filename} not found')
+        return None
+    except Exception as e:
+        print(f'Error reading file {filename}: {e}')
+        return None
