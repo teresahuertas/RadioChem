a22ca7e91be182fd308108e4e23c852bec2e9ac8
Teresa Huertas <teresahuertas@gmail.com>
2023-10-11 14:55:26 +0100

    Test spectrum reading

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import catalogues as cat
+import spectrumfit as sf
 
 #rrls = cat.Catalogue('rrls')
 #print(rrls.catalogue)
-ic418 = cat.Catalogue('IC418')
-print(ic418.rrls)
+#ic418 = cat.Catalogue('IC418')
+#print(ic418.rrls)
 #print(ic418.molecules)
 #print(ic418.uf)
+
+spectrum = sf.read_spectrum('IC418_3mm_tmb_fit_velocity.txt')
+print(spectrum)
