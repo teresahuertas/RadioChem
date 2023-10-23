ae8226522735057ef5fb5dbb3bfd87143364e916
Teresa Huertas <teresahuertas@gmail.com>
2023-10-23 16:11:40 +0100

    Add a function to read data from synthetic spectra

--- spectrumfit.py
+++ spectrumfit.py
@@ -48,6 +48,33 @@ def read_spectrum(n, filename):
     except Exception as e:
         print(f'Error reading file {filename}: {e}')
         return None
+    
+
+def read_synthetic_spectra(filename):
+    """
+    Function to read a synthetic spectrum from a file
+
+    Parameters
+    ----------
+    filename : str
+        Name of the file to read
+
+    Returns
+    -------
+    data : pandas.DataFrame
+        Data read from the file
+    """
+    try:
+        path = '/Users/teresahuertas/Documents/IAC/Proyectos/Espectros_Sinteticos/GaoLei_Spectra/'
+        data = pd.read_csv(path + filename, sep='\s+', header=None, names=['Freq[MHz]', 'Aij'], skiprows=8)
+        data.astype({'Freq[MHz]': 'float64', 'Aij': 'float64'}).dtypes
+        return data.reset_index(drop=True)
+    except FileNotFoundError:
+        print(f'File {filename} not found')
+        return None
+    except Exception as e:
+        print(f'Error reading file {filename}: {e}')
+        return None
 
 
 def create_spectrum(data, restfreq, vel):
@@ -84,10 +111,10 @@ def create_spectrum(data, restfreq, vel):
         vel = vel * u.km / u.s                          
 
     # Create spectrum
-    spectrum = Spectrum1D(flux=flux, spectral_axis=frequency,
-                            velocity_convention='radio', 
-                            rest_value=restfreq, 
-                            radial_velocity=vel)
+    spectrum = Spectrum1D(flux=flux, spectral_axis=frequency, 
+                          velocity_convention='radio', 
+                          rest_value=restfreq, 
+                          radial_velocity=vel)
     
     print('snr: ', snr_derived(spectrum))
 
--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import catalogues as cat
-from tqdm import tqdm
-#import spectrumfit as sf
+#from tqdm import tqdm
+import spectrumfit as sf
 
 # Set the number of catalogues to work with
 ncat = 3
 
-for i in tqdm (range (ncat), desc='Reading catalogues...'):
+'''for i in tqdm (range (ncat), desc='Reading catalogues...'):
     rrls = cat.Catalogue('rrls')
     ic418 = cat.Catalogue('IC418')
     ngc7027 = cat.Catalogue('NGC_7027')
-print('Catalogues read successfully!')
+print('Catalogues read successfully!')'''
+rrls = cat.Catalogue('rrls')
+ic418 = cat.Catalogue('IC418')
+ngc7027 = cat.Catalogue('NGC_7027')
 
 #print(rrls.catalogue)
 #ic418 = cat.Catalogue('IC418')
@@ -19,17 +22,20 @@ print('Catalogues read successfully!')
 #print(ngc7027.molecules)
 #print(ngc7027.uf)
 
-#files = ['IC418_3mm_tmb_fit_velocity.txt']#, 'IC418_2mm_tmb_fit_velocity.txt']
+files = ['IC418_3mm_tmb_fit_velocity.txt']#, 'IC418_2mm_tmb_fit_velocity.txt']
+C60CNp = sf.read_synthetic_spectra('C60CN+/C60CN+.dat')
+#print(C60CNp)
 
-#spectrum = sf.read_spectrum(1, files)
+spectrum = sf.read_spectrum(1, files)
 #print(spectrum)
-#spectrum = sf.create_spectrum(spectrum, 87317.0, 42.59999847412109)
+spectrum = sf.create_spectrum(spectrum, 87317.0, 42.59999847412109)
 #print(spectrum.unit)
 
 #lines = sf.line_inspector(spectrum, 0.002, 'IC418', 'emission')
 #print(lines)
 
 #sf.plot_spectrum(spectrum, lines)
+#sf.plot_spectrum(spectrum, C60CNp)
 
 #freq = sf.line_freq_width(87317.0, 60)
 #print(freq)
